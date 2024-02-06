69695abd2b8cd7859ad3fc6a3598e76a8de83cc7
GenMagadan <genchik111@yandex.ru>
2024-02-06 11:14:09 +0300

    Add solution

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
+const pathToJson = path.join(__dirname, 'pageViews.json');
+
 app.get('/', (req, res) => {
+  const viewHome = JSON.parse(fs.readFileSync(pathToJson, 'utf-8'));
+
+  viewHome.countHome = ++viewHome.countHome;
+
+  fs.writeFileSync(pathToJson, JSON.stringify(viewHome, null, 2));
+
   res.send(`
-   <h1>Главная страница</h1>
-   <a href='/about'>На вторую страницу</a>
-   `);
+  <h1>Главная страница сайта</h1>
+  <a href="/about">На следующюю</a>
+  <h3>Просмотров: ${viewHome.countHome}</h3>
+  `);
 });
 
 app.get('/about', (req, res) => {
-  res.send(`
-    <h1>Вторая страница</h1>
-    <a href='/'>На главную страницу</a>
-    `);
-});
+  const viewAbout = JSON.parse(fs.readFileSync(pathToJson, 'utf-8'));
 
+  viewAbout.countAbout = ++viewAbout.countAbout;
+
+  fs.writeFileSync(pathToJson, JSON.stringify(viewAbout, null, 2));
+
+  res.send(
+    `
+    <h1>Страница о чём-то таком</h1>
+    <a href="/">На главную страницу</a>
+    <h3>Просмотров: ${viewAbout.countAbout}</h3>
+    `
+  );
+});
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Запущен на порту ${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
